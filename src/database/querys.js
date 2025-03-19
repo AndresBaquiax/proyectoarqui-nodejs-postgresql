4312c39a4a5f40ee850b487aaf29db08d6ec3006
Enrique <enrique.armando224@gmail.com>
2025-03-19 13:21:43 -0600

    Subiendo CRUD de clientes con borrado lógico

--- src/database/querys.js
+++ src/database/querys.js
@@ -4,3 +4,11 @@ export const querysEmpleados = {
     putEmpleado: "UPDATE empleado SET nombre = $1, apellido = $2, telefono = $3, email = $4, cargo = $5 WHERE idempleado = $6",
     deleteEmpleado: "DELETE FROM empleado WHERE id = $1"
 };
+
+export const querysClientes = {
+    getClientes: 'SELECT * FROM cliente ORDER BY idcliente',
+    getClienteById: 'SELECT * FROM cliente WHERE idcliente = $1',
+    createCliente: 'INSERT INTO cliente (nombre, apellido, nit, telefono, email, status, created_at, updated_at) VALUES ($1, $2, $3, $4, $5, $6, NOW(), NOW()) RETURNING *',
+    updateCliente: 'UPDATE cliente SET nombre = $1, apellido = $2, nit = $3, telefono = $4, email = $5, status = $6, updated_at = NOW() WHERE idcliente = $7 RETURNING *',
+    deleteCliente: 'UPDATE cliente SET status = 0, updated_at = NOW() WHERE idcliente = $1'
+};
